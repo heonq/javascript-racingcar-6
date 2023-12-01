8099c8c6d4dad027fa599c3f62c50fbc86c8d54f
heonq <gkagjsrb@naver.com>
2023-12-01 15:03:42 +0900

    feat: 0에서 9사이의 무작위 값을 뽑고 4 이상일 경우 이동하는 기능 구현

---
+++ src/Car.js
@@ -0,0 +1,20 @@
+import { Random } from '@woowacourse/mission-utils';
+import VALUES from '../constants/Values.js';
+
+class Car {
+  #name;
+
+  #distance;
+
+  constructor(name) {
+    this.#name = name;
+    this.#distance = 0;
+  }
+
+  decideMoveOrStay() {
+    const value = Random.pickNumberInRange(VALUES.minRandomValue, VALUES.maxRandomValue);
+    if (value >= VALUES.minMoveValue) this.#distance += 1;
+  }
+}
+
+export default Car;
